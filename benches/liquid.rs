6f755d5d644ba250d40ef041d23d99cad6eca11e
Ed Page <eopage@gmail.com>
2018-12-31 18:44:56 -0700

    perf: Backport 0.15's benchmarks

--- benches/liquid.rs
+++ benches/liquid.rs
@@ -1,7 +1,7 @@
 #![feature(test)]
 
-extern crate test;
 extern crate serde_yaml;
+extern crate test;
 
 extern crate liquid;
 
@@ -9,96 +9,23 @@ static TEXT_ONLY: &'static str = "Hello World";
 
 #[bench]
 fn bench_parse_text(b: &mut test::Bencher) {
-    let parser = liquid::ParserBuilder::with_liquid().extra_filters().build();
+    let parser = liquid::ParserBuilder::with_liquid()
+        .extra_filters()
+        .build();
     b.iter(|| parser.parse(TEXT_ONLY));
 }
 
 #[bench]
 fn bench_render_text(b: &mut test::Bencher) {
-    let parser = liquid::ParserBuilder::with_liquid().extra_filters().build();
+    let parser = liquid::ParserBuilder::with_liquid()
+        .extra_filters()
+        .build();
     let template = parser
         .parse(TEXT_ONLY)
         .expect("Benchmark template parsing failed");
 
     let data = liquid::Object::new();
 
-    b.iter(|| template.render(&data));
-}
-
-// Mirrors tera's VARIABLE_ONLY benchmark
-static VARIABLE_ONLY: &'static str = "{{product.name}}";
-static VARIABLE_ONLY_OBJECT: &'static str = "
-username: bob
-product:
-  - name: Moto G
-  - manufacturer: Motorola
-  - summary: A phone
-  - price: 100
-";
-
-#[bench]
-fn bench_parse_variable(b: &mut test::Bencher) {
-    let parser = liquid::ParserBuilder::with_liquid().extra_filters().build();
-    b.iter(|| parser.parse(VARIABLE_ONLY));
-}
-
-#[bench]
-fn bench_render_variable(b: &mut test::Bencher) {
-    let parser = liquid::ParserBuilder::with_liquid().extra_filters().build();
-    let template = parser
-        .parse(VARIABLE_ONLY)
-        .expect("Benchmark template parsing failed");
-
-    let data: liquid::Object = serde_yaml::from_str(VARIABLE_ONLY_OBJECT)
-        .expect("Benchmark object parsing failed");
-
-    b.iter(|| template.render(&data));
-}
-
-// Mirrors handlebars' benchmark
-static ITERATE: &'static str = "<html>
-  <head>
-    <title>{{year}}</title>
-  </head>
-  <body>
-    <h1>CSL {{year}}</h1>
-    <ul>
-    {% for team in teams %}
-      <li class=\"champion\">
-      <b>{{team.name}}</b>: {{team.score}}
-      </li>
-    {{/each}}
-    </ul>
-  </body>
-</html>";
-static ITERATE_OBJECT: &'static str = "
-year: 2015
-teams:
-  - name: Jiangsu
-    score: 43
-  - name: Beijing
-    score: 27
-  - name: Guangzhou
-    score: 22
-  - name: Shandong
-    score: 12
-";
-
-#[bench]
-fn bench_parse_template(b: &mut test::Bencher) {
-    let parser = liquid::ParserBuilder::with_liquid().extra_filters().build();
-    b.iter(|| parser.parse(ITERATE));
-}
-
-#[bench]
-fn bench_render_template(b: &mut test::Bencher) {
-    let parser = liquid::ParserBuilder::with_liquid().extra_filters().build();
-    let template = parser
-        .parse(ITERATE)
-        .expect("Benchmark template parsing failed");
-
-    let data: liquid::Object = serde_yaml::from_str(ITERATE_OBJECT)
-        .expect("Benchmark object parsing failed");
-
+    template.render(&data).unwrap();
     b.iter(|| template.render(&data));
 }
